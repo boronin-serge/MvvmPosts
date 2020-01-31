bbc1c60a39846510e7183f83dc0a7b8e38c3bb1d
sboronin <Sergey.Boronin@bia-tech.ru>
2020-01-31 15:48:34 +0300

    let dagger make retrofit instance

---
+++ app/src/main/java/ru/boronin/mvvmposts/injection/module/NetworkModule.kt
@@ -0,0 +1,45 @@
+package ru.boronin.mvvmposts.injection.module
+
+import dagger.Module
+import dagger.Provides
+import dagger.Reusable
+import io.reactivex.schedulers.Schedulers
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+import ru.boronin.mvvmposts.network.PostApi
+import ru.boronin.mvvmposts.utils.BASE_URL
+
+/**
+ * Created by Sergey Boronin on 31.01.2020.
+ */
+
+@Module
+object NetworkModule {
+    /**
+     * Provides the Post service implementation.
+     * @param retrofit the Retrofit object used to instantiate the service
+     * @return the Post service implementation.
+     */
+    @Provides
+    @Reusable
+    @JvmStatic
+    internal fun providePostApi(retrofit: Retrofit): PostApi {
+        return retrofit.create(PostApi::class.java)
+    }
+
+    /**
+     * Provides the Retrofit object.
+     * @return the Retrofit object
+     */
+    @Provides
+    @Reusable
+    @JvmStatic
+    internal fun provideRetrofitInterface(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
+            .build()
+    }
+}
