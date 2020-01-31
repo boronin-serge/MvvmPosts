b9497d2f4b5f9c271310060d1a04adecfc2defa1
sboronin <Sergey.Boronin@bia-tech.ru>
2020-01-31 15:37:03 +0300

    add base classes

---
+++ app/src/main/java/ru/boronin/mvvmposts/base/BaseViewModel.kt
@@ -0,0 +1,9 @@
+package ru.boronin.mvvmposts.base
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * Created by Sergey Boronin on 31.01.2020.
+ */
+abstract class BaseViewModel : ViewModel() {
+}
---
+++ app/src/main/java/ru/boronin/mvvmposts/model/Post.kt
@@ -0,0 +1,6 @@
+package ru.boronin.mvvmposts.model
+
+/**
+ * Created by Sergey Boronin on 31.01.2020.
+ */
+data class Post(val userId: Int, val id: Int, val title: String, val body: String)
